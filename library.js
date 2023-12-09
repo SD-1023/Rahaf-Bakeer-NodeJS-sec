3c487ebcfa955d9239ff0a2dd30e2e65567772d3
Rahaf0B <rahafbakeer123@gmail.com>
2023-12-10 00:43:15 +0200

    fixing and updating

--- library.js
+++ library.js
@@ -5,22 +5,30 @@ const services = require("./services.js");
 const e = require("express");
 
 routeLibrary.get("/:id", async (req, res) => {
-  if (isNaN(req.params.id)){
-    res.status(404).send( { message: "Invalid id" });
-  }
-  else{
-  const book = await services.getBookWithID(parseInt(req.params.id));
-  res
-    .status(book?.success === true ? 200 : 204)
-    .send(book?.success ? { book: book.book } : { message: book.message });
+  if (isNaN(req.params.id)) {
+    res.status(404).send({ message: "Invalid id" });
+  } else {
+    try {
+      const book = await services.getBookWithID(parseInt(req.params.id));
+
+      if (book) {
+        res.render("book", { status: 200, data: book });
+      } else {
+        res.render("book", { status: 400, message: "book not found" });
+      }
+    } catch (e) {
+      res.render("book", { status: 400, message: e.message });
+    }
   }
 });
 
 routeLibrary.get("/", async (req, res) => {
-  const books = await services.getBooks();
-  res.render("books",{"status":books?.success === true ? 200 : 500,"data":books?.success === true ? books : { message: books.message }});
-    // res.status(books?.success === true ? 200 : 204)
-    // .send(books?.success === true ? books : { message: books.message });
+  try {
+    const books = await services.getBooks();
+    res.render("books", { status: 200, data: books });
+  } catch (e) {
+    res.render("books", { status: 400, message: e.message });
+  }
 });
 
 //POST
@@ -29,16 +37,16 @@ routeLibrary.use(bodyParser.urlencoded({ extended: false }));
 
 routeLibrary.post("/", async (req, res) => {
   let data = req.body;
-  if (data.name){
-  const book = await services.addBooks(data);
-  res
-    .status(book?.success === true ? 200 : 500)
-    .send({ message: book.message });
-  }
-    else{
-      res.status( 400)
-      .send({ message: "the book name should be provided"});
+  if (data.name && typeof data?.name === "string") {
+    try {
+      const response = await services.addBooks(data);
+      res.status(200).send({ message: response.message });
+    } catch (e) {
+      res.status(400).send({ message: e.message });
     }
-  });
+  } else {
+    res.status(400).send({ message: "check the data" });
+  }
+});
 
 module.exports = routeLibrary;
--- services.js
+++ services.js
@@ -1,107 +1,67 @@
 const fs = require("fs/promises");
 const fse = require("fs-extra");
-
+const booksFile = "books.json";
 async function readFile(file) {
   try {
     const fileData = await fs.readFile(file, "utf-8");
-    return [JSON.parse(fileData), "data found"];
+    return JSON.parse(fileData);
   } catch (e) {
-    let errorMSG = "";
-    if (e.code === "ENOENT") {
-      errorMSG = "file not found";
-      console.log("no such file or directory " + file);
-    } else {
-      console.log(e.message);
-      errorMSG = e.message;
-    }
-    return [[], errorMSG];
-  }
-}
-
-async function checkFileExists(file) {
-  try {
-    await fse.ensureFile(file);
-  } catch (e) {
-    console.log(e.message);
+    throw new Error(e.message);
   }
 }
 
 function checkBookExists(books, checkingProps, value) {
   try {
     const book = books?.filter((book) => book[checkingProps] === value);
-    if (book?.length === 0) {
-      return [[], "book not found"];
-    } else {
-      return [book, "book found"];
-    }
+    return book;
   } catch (e) {
-    console.log(e.message);
-    return [[], e.message];
+    throw new Error(e.message);
   }
 }
 
 async function getBooks() {
   try {
-    const [books, errorMSG] = await readFile("books.json");
+    const books = await readFile(booksFile);
     return {
       books: books?.books,
-      success: books?.books ? true : false,
-      message: errorMSG,
     };
   } catch (e) {
-    console.log(e.message);
-    return { success: false, message: e.message };
+    throw new Error(e.message);
   }
 }
 
 async function getBookWithID(id) {
   try {
-    const [books, errorMSG] = await readFile("books.json");
-    if (books.books?.length > 0) {
-      const [book, errorMSG2] =
-        books?.length !== 0 ? checkBookExists(books["books"], "id", id) : [];
-      return {
-        book: book,
-        success: book?.length > 0 ? true : false,
-        message: errorMSG2,
-      };
-    }
-    return { book: [], success: false, message: errorMSG };
+    const books = await readFile(booksFile);
+
+    const book = checkBookExists(books.books, "id", id);
+    return book[0];
   } catch (e) {
-    console.log(e);
-    return { success: false, message: e.message };
+    throw new Error(e.message);
   }
 }
 
 async function addBooks(dataInfo) {
   try {
-    checkFileExists("books.json");
-    var [books, errorMSG] = await readFile("books.json");
-    if (books?.length < 0) {
-      return { success: false, message: errorMSG };
-    }
-
-    const [book, , errorMSG2] =
-      books?.length !== 0
-        ? checkBookExists(books["books"], "name", dataInfo.name)
-        : [];
-    if (book?.length > 0) {
-      console.log("The Book already exists");
-
-      return { success: false, message: "The Book already exists" };
-    } else {
-      id = books?.length !== 0 ? books?.books?.at(-1).id + 1 : 1;
+    var books;
+    try {
+      books = await readFile(booksFile);
+    } finally {
+      const book = checkBookExists(books?.books, "name", dataInfo.name);
+      if (book?.length > 0) {
+        throw new Error("The Book already exists");
+      }
+      id = books ? books?.books?.at(-1).id + 1 : 1;
       let data = {};
       data["id"] = id;
       data["name"] = dataInfo.name;
       books?.books?.push(data);
-      const dataToWrite = books?.length !== 0 ? books : { books: [data] };
-      fs.writeFile("books.json", JSON.stringify(dataToWrite));
-      return { success: true, message: "The data has been updated" };
+      const dataToWrite = books ? books : { books: [data] };
+      fs.writeFile(booksFile, JSON.stringify(dataToWrite));
+      return { message: "The data has been updated" };
     }
   } catch (e) {
-    console.log(e.message);
-    return { success: false, message: "error has occurred" + e.message };
+    throw new Error(e.message);
   }
 }
 
